491cf9e92b565375d5d7491011c7fa0a8f2b78d5
Sarah Glenn <sarahadaline@gmail.com>
2024-10-29 07:31:24 +0100

    setting up auth routes and installing passport

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const router = require('express').Router();
 router.get('/', (req, res) => {
   res.send('All Things Running Store API');
 });
+
+router.use('/auth', require('./auth'));
 router.use('/users', require('./users'));
 router.use('/products', require('./products'));
 router.use('/orders', require('./orders'));
